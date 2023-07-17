dfb7bd1024b7951377a924f8ec4d20ca36ab6e3f
Alex King <calicoalix@gmail.com>
2023-07-17 21:56:07 +1000

    createpostcomponent created and header configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import './App.css'
-import Routes from './routes'
-import HomePageSettings from './components/HomePageSettings'
+import "./App.css";
+import Routes from "./routes";
+import HomePageSettings from "./components/HomePageSettings";
+import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
 
 function App() {
+  const [currLocation, setCurrLocation] = useState<string>("home");
+  const location = useLocation();
+
+  const getPageTitle = () => {
+    switch (location.pathname) {
+      case '/':
+        return 'Home';
+      case '/login':
+        return 'Login';
+      case '/signup':
+        return 'Signup';
+      case '/search':
+        return 'Search Results';
+      case '/search/user':
+        return 'User Results';
+      case '/test':
+        return 'S3 Test';
+      case '/accounts/settings':
+        return 'Settings';
+        default:
+          if (location.pathname.startsWith('/')) {
+            const username = location.pathname.slice(1); 
+            return `${username}'s profile`; 
+          }
+          return '';
+    }
+  }
 
   return (
     <div className="App">
       <div className="settings">
-        <HomePageSettings/>
+        <HomePageSettings />
       </div>
-      <Routes/>
+      <div className="content-side">
+        <div className="location-title">{getPageTitle()}</div>
+        <Routes />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CreatePostComponent.tsx
+++ src/components/CreatePostComponent.tsx
@@ -1,11 +1,54 @@
-import './CreatePostComponent.css'
+import "./CreatePostComponent.css";
 
-function createPostComponent() {
-    return (
-        <div className="CreatePost">
-            Create Post
+import React, { useState, useRef, useEffect } from "react";
+import axios from 'axios';
+
+const createPostComponent = () => {
+  const [text, setText] = useState<string>("");
+  const [remainingChars, setRemainingChars] = useState<number>(256);
+  const [focused, setFocused] = useState<boolean>(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = event.target.value;
+    const remaining = 256 - inputValue.length;
+
+    if (remaining >= 0) {
+      setText(inputValue);
+      setRemainingChars(remaining);
+    }
+  };
+  const handlePost = async () => {
+    const token = localStorage.getItem("token");
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    await axios.post("http://localhost:8082/post", {post_content: text}, {headers})
+    .then(res => {
+        console.log(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+  }
+
+  return (
+    <div className="CreatePost">
+      <div className="compose-wrapper">
+        <div className="write-and-submit-wrapper">
+          <textarea
+            className="compose-post"
+            value={text}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(true)}
+            onChange={handleChange}
+            placeholder="wazzup?"
+          />
+          <div className="submit-post" onClick={handlePost}>Post</div>
         </div>
-    )
-}
+        {focused && <div className="counter">{text.length} / 256</div>}
+      </div>
+    </div>
+  );
+};
 
-export default createPostComponent
+export default createPostComponent;
